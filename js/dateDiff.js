fabe02ee1bc659ab56ec58acdeb650c39cae8c70
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2019-03-16 14:42:23 +0100

    added TimeLeft class

--- js/dateDiff.js
+++ js/dateDiff.js
@@ -1,3 +1,31 @@
+function TimeLeft(days, hours, minutes, seconds) {
+
+    this.days = days;
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+
+    this.format = function() {
+
+        let days = this.days;
+        let hours = this.hours;
+        let minutes = this.minutes;
+        let seconds = this.seconds;
+
+        if(days < 10) days = "0" + days;
+        if(hours < 10) hours = "0" + hours;
+        if(minutes < 10) minutes = "0" + minutes;
+        if(seconds < 10) seconds = "0" + seconds;
+
+        return {
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        }
+    }
+}
+
 $.fn.diff = function(date) {
 
     let dateEl = $(this).get()[0];
@@ -18,11 +46,5 @@ $.fn.diff = function(date) {
     minutes = minutes-(days*24*60)-(hours*60);
     seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
 
-    return {
-        days: days,
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds
-    }
-
+    return new TimeLeft(days, hours, minutes, seconds);
 };
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     let dateFuture = new Date(2019, 5, 21);
     let dateNow = new Date();
 
-    let diff = $(dateNow).diff(dateFuture);
+    let diff = $(dateNow).diff(dateFuture).format();
 
     $("#days").html(diff.days);
     $("#hours").html(diff.hours);
