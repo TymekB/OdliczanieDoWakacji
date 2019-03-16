9f4921f79a53267fcc638b337a31f64574fc27ee
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2019-03-16 15:30:52 +0100

    added progress

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-$(document).ready(function(){
+function showTimeLeft() {
+    let startDate = new Date(2018, 8, 2);
+    let endDate = new Date(2019, 5, 21);
 
-    let dateFuture = new Date(2019, 5, 21);
-    let dateNow = new Date();
+    let totalDays = $(startDate).diff(endDate);
+    let daysLeft = $(new Date()).diff(endDate).format();
+    let progress = Math.floor(((totalDays.days - daysLeft.days ) / totalDays.days) * 100);
 
-    let diff = $(dateNow).diff(dateFuture).format();
+    $("#days").html(daysLeft.days);
+    $("#hours").html(daysLeft.hours);
+    $("#minutes").html(daysLeft.minutes);
 
-    $("#days").html(diff.days);
-    $("#hours").html(diff.hours);
-    $("#minutes").html(diff.minutes);
+    $("#progress").html(progress + "%");
+    $("#progress-bar").val(progress);
+}
+
+$(document).ready(function() {
+
+    showTimeLeft();
 
 });
