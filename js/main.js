602da12ebc76499f41fddd8e7bfb7f31a057e6c5
TymekB <baran.tymoteusz@gmail.com>
2019-03-16 16:51:03 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function showTimeLeft() {
+    let startDate = new Date(2018, 8, 2);
+    let endDate = new Date(2019, 5, 21);
+
+    let totalDays = $(startDate).diff(endDate);
+    let daysLeft = $(new Date()).diff(endDate).format();
+    let progress = Math.floor(((totalDays.days - daysLeft.days ) / totalDays.days) * 100);
+
+    $("#days").html(daysLeft.days);
+    $("#hours").html(daysLeft.hours);
+    $("#minutes").html(daysLeft.minutes);
+
+    $("#progress").html(progress + "%");
+    $("#progress-bar").val(progress);
+}
+
+$(document).ready(function() {
+
+    showTimeLeft();
+    setInterval(showTimeLeft, 1000);
+
+    let mainTitleHtml = $('#main-title').html();
+
+    $(window).resize(function() {
+        let width = $(window).width();
+
+        if (width < 525) {
+
+            let newTitle = mainTitleHtml.substr(0, mainTitleHtml.length-3);
+            $('#main-title').html(newTitle);
+
+        } else {
+            $('#main-title').html(mainTitleHtml);
+        }
+    });
+});
