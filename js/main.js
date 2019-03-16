3179cdf04334ca6f89558ff3d8b8c685e4ecb418
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2019-03-16 13:48:27 +0100

    added timer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
-    console.log('working');
+    let dateFuture = new Date(2019, 5, 21);
+    let dateNow = new Date();
+
+    let seconds = Math.floor((dateFuture - (dateNow))/1000);
+    let minutes = Math.floor(seconds/60);
+    let hours = Math.floor(minutes/60);
+    let days = Math.floor(hours/24);
+
+    hours = hours-(days*24);
+    minutes = minutes-(days*24*60)-(hours*60);
+    seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+
+    $("#days").html(days);
+    $("#hours").html(hours);
+    $("#minutes").html(minutes);
 
 });
