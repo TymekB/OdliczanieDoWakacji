ae4139a6b690d85b491efa5b0af80cd351b08554
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2019-10-12 14:33:03 +0200

    Updates

---
+++ js/app.js
@@ -0,0 +1,40 @@
+let app = new Vue({
+	el: "#countdown",
+	data: {
+		startDate: new Date(new Date().getFullYear(), 8, 2),
+		endDate: new Date(new Date().getFullYear()+1, 5, 27),
+		dateDifference: null,
+		progress: null
+	},
+	methods: {
+		countDateDifference(start, end) {
+
+			const units = countdown.DAYS | countdown.HOURS | countdown.MINUTES;
+			let diff = countdown(end, null, units);
+
+			if (diff.days < 10) diff.days = "0" + diff.days;
+			if (diff.hours < 10) diff.hours = "0" + diff.hours;
+			if (diff.minutes < 10) diff.minutes = "0" + diff.minutes;
+
+			return diff;
+
+		},
+		countProgress(start, end) {
+			let total = countdown(end, start, countdown.MILLISECONDS).milliseconds;
+			let fromNow = countdown(end, null, countdown.MILLISECONDS).milliseconds;
+			let progress = (1 - (fromNow / total)) * 100;
+
+			return progress.toFixed(1);
+		}
+	},
+	created() {
+
+		this.dateDifference = this.countDateDifference(this.startDate, this.endDate);
+		this.progress = this.countProgress(this.startDate, this.endDate);
+
+		setInterval(() => {
+			this.dateDifference = this.countDateDifference(this.startDate, this.endDate);
+			this.progress = this.countProgress(this.startDate, this.endDate);
+		}, 1000);
+	}
+});
